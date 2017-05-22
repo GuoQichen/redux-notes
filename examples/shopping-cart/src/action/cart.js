795f3fcd4b6d03b724d2bccdd68a61d0444243c0
GuoQichen <250915274@qq.com>
2017-05-22 18:10:43 +0800

    update cart action and reducer

--- examples/shopping-cart/src/action/cart.js
+++ examples/shopping-cart/src/action/cart.js
@@ -2,12 +2,13 @@ import { CART_CHECKOUT } from '../constant/ActionTypes'
 
 export const getCartProduct = (state) => state.cart.productId.map(id => state.products.productById[id])
 export const getCartQuantity = (state, id) => state.cart.quantity[id]
-export const getTotalMoney = (state) => {
-    const total = getCartProduct(state).reduce((total, product) => { 
-        return total += product.price * getCartQuantity(state, product.id)
-    }, 0)
-    return total.toFixed(2)
-}
+export const getTotalMoney = (state) =>
+     getCartProduct(state).reduce((total, product) =>
+        total += product.price * getCartQuantity(state, product.id),
+        0
+    )
+    .toFixed(2)
+
 export const checkout = () => ({
     type: CART_CHECKOUT
 })
--- examples/shopping-cart/src/reducer/cart.js
+++ examples/shopping-cart/src/reducer/cart.js
@@ -1,32 +1,40 @@
 import { combineReducers } from 'redux'
 import { ADD_TO_CART, CART_CHECKOUT } from '../constant/ActionTypes'
 
-const productId = (state = [], action) => {
+const initState = {
+    productId: [],
+    quantity: {}
+}
+
+const productId = (state = initState.productId, action) => {
     switch (action.type) {
         case ADD_TO_CART:
             return state.indexOf(action.payload) !== -1 ? state : [...state, action.payload]
-        case CART_CHECKOUT:
-            return []
         default:
             return state
     }
 }
 
-const quantity = (state = {}, action) => {
+const quantity = (state = initState.quantity, action) => {
     switch (action.type) {
         case ADD_TO_CART:
             return {
                 ...state,
                 [action.payload]: state[action.payload] ? ++state[action.payload] : 1
-            }
-        case CART_CHECKOUT:
-            return {}            
+            }          
         default: 
             return state
     }
 }
 
-export default combineReducers({
-    productId,
-    quantity
-})
+export default function cartReducer(state = initState, action) {
+    switch (action.type) {
+        case CART_CHECKOUT:
+            return initState
+        default:
+            return {
+                productId: productId(state.productId, action),
+                quantity: quantity(state.quantity, action)
+            }
+    }
+}
