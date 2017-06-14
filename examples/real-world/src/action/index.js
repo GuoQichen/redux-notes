162f804ad1b286122e9f657fb1fc745f6b6abc65
guoqichen <250915274@qq.com>
2017-06-14 23:49:14 +0800

    modify fetchRepos

--- examples/real-world/src/action/index.js
+++ examples/real-world/src/action/index.js
@@ -23,7 +23,8 @@ export const fetchUser = (userName) => (dispatch, getState) =>  {
 }
 
 export const fetchRepos = () => (dispatch, getState) => {
-    const { repos_url } = getState().user
+    const { repos_url } = getState().user.data
+    if (!repos_url) return
     dispatch({ type: REPO_REQUEST })
     return fetch(repos_url)
         .then(result => 
