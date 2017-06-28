4f43097e086a34d342dc0c591aa402e5d650d0b5
guoqichen <250915274@qq.com>
2017-06-29 00:15:33 +0800

    update middleware

--- examples/real-world/src/middleware.js
+++ examples/real-world/src/middleware.js
@@ -1,36 +1,47 @@
-const callApiMiddleware = ({ dispatch, getstate }) => next => action => {
-    if (Array.isArray(action.types) && action.types.length === 3 && action.types.every(item => typeof item === 'string')) {
-        const {
-            types,
-            shouldCallApi = () => true,
-            callApi,
-            payload = {}
-        } = action
-        const [ request, success, failure ] = types
-        dispatch({
-            type: request,
-            payload,
-        })
-        if (!shouldCallApi()) {
-            return
-        }
-        return callApi().then(response => 
-            response.json().then(result => 
-                dispatch({
-                    type: success,
-                    response: result,
-                    payload,
-                })
-            )
-        ).catch(error => 
+const callApiMiddleware = ({ dispatch, getState }) => next => action => {
+    const {
+        types,
+        shouldCallApi = () => true,
+        callApi,
+        payload = {}
+    } = action
+    if (!types) {
+      return next(action)
+    }
+    if (
+      !Array.isArray(types) ||
+      types.length !== 3 ||
+      !types.every(type => typeof type === 'string')
+    ) {
+      throw new Error('Expected an array of three string types.')
+    }
+    if (typeof callApi !== 'function') {
+      throw new Error('Expected callApi to be a function.')
+    }
+    if (!shouldCallApi(getState())) {
+        return
+    }
+    const [ request, success, failure ] = types
+    dispatch({
+        type: request,
+        ...payload,
+    })
+    return callApi().then(response => 
+        response.json().then(result => 
             dispatch({
-                type: failure,
-                error
+                type: success,
+                response: result,
+                ...payload,
             })
         )
-    }
+    ).catch(error => 
+        dispatch({
+            type: failure,
+            error,
+            ...payload
+        })
+    )
 
-    return next(action)
 }
 
 export default callApiMiddleware
